3ccc56311610885826e1a76a504c9cd7529f7560
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2026-02-04 16:41:24 +0100

    Testing Demo Sessions

--- ai_agent_sdk.js
+++ ai_agent_sdk.js
@@ -1,1 +1,13 @@
-console.log('HEYYYYYY')
+(function() {
+  const siteKey = document.currentScript.dataset.key;
+  let sessionId = localStorage.getItem("ai_session");
+  if (!sessionId) {
+    sessionId = crypto.randomUUID();
+    localStorage.setItem("ai_session", sessionId);
+  }
+
+  console.log("AI Agent SDK loaded");
+  console.log("Site Key:", siteKey);
+  console.log("Session ID:", sessionId);
+
+})();
